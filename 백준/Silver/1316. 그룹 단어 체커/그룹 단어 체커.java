1455a4f028ebefd8128d20e0ce491a5a744d106a
Park Junyeong <bulruboa@gmail.com>
2022-05-06 20:02:23 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 220 ms, Memory: 17776 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        int count = 0;
+
+        loop:
+        for(int i = 0; i < num; i++) {
+            String word = in.next();
+            ArrayList<Character> check = new ArrayList<>();
+            check.add(word.charAt(0));
+            for(int m = 1; m < word.length(); m++) {
+                char target = word.charAt(m);
+                if(!check.contains(target)) {
+                    check.add(target);
+                } else if(check.contains(target) && (target != word.charAt(m - 1))) {
+                    check = new ArrayList<>();
+                    continue loop;
+                }
+            }
+            count ++;
+        }
+        System.out.println(count);
+    }
+}
+

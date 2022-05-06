a6d379be2f69c801060f9df0275643da59e2160c
Park Junyeong <bulruboa@gmail.com>
2022-05-06 19:35:06 +0900

    [Bronze II] Title: 다이얼, Time: 216 ms, Memory: 17632 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.java
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -4,32 +4,27 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String word = in.nextLine();
-
         int time = 0;
-        for(int i = 0; i < word.length(); i++) {
-            char point = word.charAt(i);
-            int asc = (int)point;
+        for (int i = 0; i < word.length(); i++) {
+            int asc = (int)(word.charAt(i));
 
-            if('A' <= asc && asc <= 'C') {
-                time +=3;
-            } else if('D' <= asc && asc <= 'F') {
-                time +=4;
-            } else if('G' <= asc && asc <= 'I') {
-                time +=5;
-            } else if('J' <= asc && asc <= 'L') {
-                time +=6;
-            } else if('M' <= asc && asc <= 'O') {
-                time +=7;
-            } else if('P' <= asc && asc <= 'S') {
-                time +=8;
-            } else if('T' <= asc && asc <= 'V') {
-                time +=9;
-            } else if('W' <= asc && asc <= 'Z') {
-                time +=10;
+            if ('A' <= asc && asc <= 'C') {
+                time += 3;
+            } else if (asc <= 'F') {
+                time += 4;
+            } else if (asc <= 'I') {
+                time += 5;
+            } else if (asc <= 'L') {
+                time += 6;
+            } else if (asc <= 'O') {
+                time += 7;
+            } else if (asc <= 'S') {
+                time += 8;
+            } else if (asc <= 'V') {
+                time += 9;
             } else {
-                time +=0;
+                time += 10;
             }
-
         }
         System.out.println(time);
     }
